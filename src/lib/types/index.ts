aa623cfaab53f215d671f2e3a2d6804ed8349dac
Manik <manik6418@gmail.com>
2023-11-26 18:29:15 +0530

    train manager init

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -10,23 +10,6 @@ export interface Profile {
 	created_at: string;
 }
 
-export interface Job {
-	id: string;
-	company: string;
-	logo: string;
-	logoBackground: string;
-	position: string;
-	created_at: string;
-	contract: string;
-	location: string;
-	website: string;
-	apply: string;
-	description: string;
-	requirements_content: string;
-	requirements_items: string[];
-	role_content: string;
-	role_items: string[];
-}
 
 export interface EntityWithId {
 	id: string;
